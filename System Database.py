f22081a69f0cef63c24074ef675ed04f909c4cd2
Charlie White <159645979+MrCharlieWhite@users.noreply.github.com>
2026-07-28 17:00:59 +0100

    Finished database also changing the way it is formatted

--- System Database.py
+++ System Database.py
@@ -1,6 +1,23 @@
 import json
 
-scores = [100, 74, 23, 3]
+# Iterating through all dimensions in the database array
+fullDatabase = [
+    [{i: "" for i in range(5)} for _ in range(3)]
+    for _ in range(20)
+]
 
-with open("AutoSortDatabase.JSON", "w") as f:
-    json.dump(scores, f)
+# Way to easily update data within the array via narrowing parameters down
+sector = int(input("Enter sector (0-19): "))
+unit = int(input("Enter unit (0-2): "))
+module = int(input("Enter module (0-4): "))
+value = input("Enter new value: ")
+
+# Updating the array
+fullDatabase[sector][unit][module] = value
+
+# Printing changes
+print(fullDatabase[sector][unit])
+
+# Updating the JSON File
+with open("AutoSortDatabase.json", "w") as f:
+    json.dump(fullDatabase, f)
